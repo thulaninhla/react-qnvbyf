b3b95e17e948ed453d9f9f4cbc4710e559a9f07f
thulaninhla <thulaninhlapo2@gmail.com>
2021-07-30 00:53:04 +0200

    1 file modified

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -62,7 +62,39 @@ export default function Todo() {
     }
   }
 
- 
+  function Todo({ todo, index, markTodo, removeTodo }) {
+    return (
+      <div className="todo" >
+        <span style={{ textDecoration: todo.isDone ? "line-through" : "" }}>{todo.text}</span>
+        <div>
+          <Button variant="outline-success" onClick={() => markTodo(index)}>✓</Button>{' '}
+          <Button variant="outline-danger" onClick={() => removeTodo(index)}>✕</Button>
+        </div>
+      </div>
+    );
+  }
+  function FormTodo({ addTodo }) {
+    const [value, setValue] = React.useState("");
+  
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addTodo(value);
+      setValue("");
+    };
+  
+    return (
+      <Form onSubmit={handleSubmit}> 
+      <Form.Group>
+        <Form.Label><b>Add Todo</b></Form.Label>
+        <Form.Control type="text" className="input" value={value} onChange={e => setValue(e.target.value)} placeholder="Add new todo" />
+      </Form.Group>
+      <Button variant="primary mb-3" type="submit">
+        Submit
+      </Button>
+    </Form>
+    );
+  }
 
   return (
 
